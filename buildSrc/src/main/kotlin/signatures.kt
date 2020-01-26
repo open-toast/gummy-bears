2c20fff287a5c441682ffa05ebe7e95be9031cc4
Oleg Golberg <ogolberg@toasttab.com>
2020-01-26 10:50:05 -0500

    fixup

--- buildSrc/src/main/kotlin/signatures.kt
+++ buildSrc/src/main/kotlin/signatures.kt
@@ -72,6 +72,10 @@ fun Project.buildSignatures(
         dependsOn(":sugar:build")
         dependsOn(":test:sugar-calls:build")
 
+        reports {
+            junitXml.destination = file("${rootProject.buildDir}/test-results")
+        }
+
         systemProperty("sdk", sdk)
         systemProperty("jar", configurations.getByName(scopes.sugarCalls).asPath)
         systemProperty("dexout", project.buildDir)
