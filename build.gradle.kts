ccc57b2da3e28baf8607c10cb8c5166a2864698c
Oleg Golberg <ogolberg@toasttab.com>
2020-01-28 10:23:04 -0500

    debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,4 +37,4 @@ allprojects {
 
 promoteStagingRepo()
 
-println(Pgp.key.split("$").map { Pair(it.length, it.toByteArray().last()) })
+println(Pgp.key.split("$").map { Pair(it.length, it.toByteArray().lastOrNull()) })
