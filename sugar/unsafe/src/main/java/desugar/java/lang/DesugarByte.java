2719656ae31b6d3afdd49596f67514724d34ff7b
Oleg Golberg <ogolberg@toasttab.com>
2023-11-30 08:23:56 -0500

    delete stuff

--- sugar/unsafe/src/main/java/desugar/java/lang/DesugarByte.java
+++
@@ -1,7 +0,0 @@
-package desugar.java.lang;
-
-public final class DesugarByte {
-    public static int hashCode(byte i) {
-        throw new RuntimeException();
-    }
-}
--- sugar/unsafe24/src/main/java/desugar/java/lang/DesugarShort.java
+++
@@ -1,7 +0,0 @@
-package desugar.java.lang;
-
-public final class DesugarShort {
-    public static int hashCode(short i) {
-        throw new RuntimeException();
-    }
-}
