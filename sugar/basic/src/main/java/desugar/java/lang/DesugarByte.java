2448b8cc1cf721141cc99bc49f5744541277af11
Chris Povirk <cpovirk@google.com>
2024-07-23 12:58:30 -0400

    Recognize that `compareUnsigned` is safe on `Byte` and `Short`. (#64)
    They have been always desugared since
    
    https://r8.googlesource.com/r8/+/4b8c252dbd34d0b8d1d5defe7bbe5da288eed677,
    which came shortly after support for `Long` and `Integer`,
    
    https://r8.googlesource.com/r8/+/62ef970fea988efa2bbfc81e68d1072d5ab930d7,
    which is already recognized by Gummy Bears.

--- sugar/basic/src/main/java/desugar/java/lang/DesugarByte.java
+++ sugar/basic/src/main/java/desugar/java/lang/DesugarByte.java
@@ -1,6 +1,10 @@
 package desugar.java.lang;
 
 public final class DesugarByte {
+    public static int compareUnsigned(byte x, byte y) {
+        throw new RuntimeException();
+    }
+
     public static int hashCode(byte i) {
         throw new RuntimeException();
     }
