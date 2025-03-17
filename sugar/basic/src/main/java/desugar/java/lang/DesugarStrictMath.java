4004ad43dc9ca790bde6e43241d0e641ae25b830
Oleg Golberg <ogolberg@toasttab.com>
2025-03-17 09:13:09 -0400

    Add signatures for DesugarStrictMath (#93)

---
+++ sugar/basic/src/main/java/desugar/java/lang/DesugarStrictMath.java
@@ -0,0 +1,75 @@
+package desugar.java.lang;
+
+public final class DesugarStrictMath {
+    public static double nextDown(double x) {
+        throw new RuntimeException();
+    }
+
+    public static float nextDown(float x) {
+        throw new RuntimeException();
+    }
+
+    public static int addExact(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int floorDiv(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int floorMod(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int floorMod(long x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int multiplyExact(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int subtractExact(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static int toIntExact(long x) {
+        throw new RuntimeException();
+    }
+
+    public static long addExact(long x, long y) {
+        throw new RuntimeException();
+    }
+
+    public static long floorDiv(long x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static long floorDiv(long x, long y) {
+        throw new RuntimeException();
+    }
+
+    public static long floorMod(long x, long y) {
+        throw new RuntimeException();
+    }
+
+    public static long multiplyExact(long x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static long multiplyExact(long x, long y) {
+        throw new RuntimeException();
+    }
+
+    public static long multiplyFull(int x, int y) {
+        throw new RuntimeException();
+    }
+
+    public static long multiplyHigh(long x, long y) {
+        throw new RuntimeException();
+    }
+
+    public static long subtractExact(long x, long y) {
+        throw new RuntimeException();
+    }
+}
